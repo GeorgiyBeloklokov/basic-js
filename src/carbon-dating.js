d5214dd9bfa9db1629d4bdec68ee8cc2773f20d9
GeorgiyBeloklokov <georgiy.reus@gmail.com>
2022-10-16 18:20:20 +0300

    init: 3

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+	const age = Math.ceil(
+		Math.log(MODERN_ACTIVITY / +sampleActivity) /
+			(Math.log(2).toFixed(3) / HALF_LIFE_PERIOD)
+	);
+	if (
+		typeof sampleActivity !== 'string' ||
+		isNaN(age) ||
+		age < 0 ||
+		age == 'Infinity'
+	)
+		return false;
+	return age;
 }
 
 module.exports = {
